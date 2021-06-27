772dd903b0cd1967fa31badf72d1efbc7b64c8bd
Shubham Lal <shubhamlal400@gmail.com>
2021-06-27 19:10:21 +0530

    first commit

---
+++ src/main/java/com/archislearn/demo/api/HelloController.java
@@ -0,0 +1,14 @@
+package com.archislearn.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello archis";
+    }
+}
